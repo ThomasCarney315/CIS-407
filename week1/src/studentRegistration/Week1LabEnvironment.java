f449ff5ec742e7196b19f250a6e8eafe05cd599c
ThomasCarney315 <thomascarney315@gmail.com>
2024-01-03 12:48:52 -0800

    week 1 complete

---
+++ week1/src/studentRegistration/Week1LabEnvironment.java
@@ -0,0 +1,13 @@
+//Thomas Carney - January 3, 2024 CIS406 -Setting Up Your Development Environment
+package studentRegistration;
+
+public class Week1LabEnvironment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Congratulations on your first Java program of CIS407");
+	}
+
+}
